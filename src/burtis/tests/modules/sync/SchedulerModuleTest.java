2172f2266158392b02367bf9eb02e98965bb50c8
kaolpr <mikolaj.sowinski@gmail.com>
2014-12-09 16:48:43 +0100

    Major cleanup

--- src/burtis/tests/modules/sync/SchedulerModuleTest.java
+++ src/burtis/tests/modules/sync/SchedulerModuleTest.java
@@ -1,9 +1,29 @@
 package burtis.tests.modules.sync;
 
+import burtis.modules.network.NetworkConfig;
+import burtis.modules.network.client.ClientModule;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class SchedulerModuleTest
 {
-    public static void main(String[] args)
-    {
-        // TODO Auto-generated method stub
+    
+    ClientModule client = new ClientModule(NetworkConfig.defaultConfig().getModuleConfigs().get(NetworkConfig.GUI_MODULE));
+    
+    private void start() {
+        try {
+            
+            client.connect();
+            client.send(null);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(SchedulerModuleTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public static void main(String[] args) {
+        
     }
 }
