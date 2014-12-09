47b1006ec9046640b8e7b039541be7fd8e732e77
Amadeusz Sadowski <amis.tau.92@gmail.com>
2014-12-09 23:04:49 +0100

    Added DemoServer
    ready to run on defaults

---
+++ src/burtis/modules/network/server/DemoServerModule.java
@@ -0,0 +1,40 @@
+package burtis.modules.network.server;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+import burtis.modules.network.NetworkConfig;
+
+/**
+ * Demo program running Server with default configuration until any input is
+ * detected.
+ * 
+ * @author Amadeusz Sadowski
+ *
+ */
+public class DemoServerModule extends Server
+{
+    public DemoServerModule()
+    {
+        super(NetworkConfig.defaultConfig());
+    }
+
+    public static void main(String[] args)
+    {
+        DemoServerModule server = new DemoServerModule();
+        try
+        {
+            server.run();
+            System.out.println("Naciśnij enter any zakończyć.");
+            System.in.read();
+        }
+        catch (IOException ex)
+        {
+            Server.logger.log(Level.SEVERE, null, ex);
+        }
+        if (server.isRunning)
+        {
+            server.stop();
+        }
+    }
+}
