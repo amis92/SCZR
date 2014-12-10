e49afd70a4249cd8fce57a4e41fa729f16fa6e31
Amadeusz Sadowski <amis.tau.92@gmail.com>
2014-12-10 18:25:03 +0100

    touchup - no errors!

--- src/burtis/common/events/Simulation/BusStateEvent.java
+++ src/burtis/common/events/Simulation/BusStateEvent.java
@@ -1,38 +1,49 @@
 package burtis.common.events.Simulation;
 
+import java.util.List;
+
 import burtis.common.events.AbstractEventProcessor;
 import burtis.common.events.SimulationEvent;
-import burtis.common.mockups.MockupBusState;
-import java.util.List;
+import burtis.modules.simulation.models.Bus;
 
 /**
  *
  * @author Mikołaj
  */
-public class BusStateEvent extends SimulationEvent {
-    
-    public static class BusInfo {
+public class BusStateEvent extends SimulationEvent
+{
+    private static final long serialVersionUID = 7441022056679823117L;
+
+    public static class BusInfo
+    {
         public final int busId;
-        public final MockupBusState state;
+        public final Bus.State state;
         public final int lengthPassed;
 
-        public BusInfo(int busId, MockupBusState state, int lengthPassed) {
+        public BusInfo(int busId, Bus.State state, int lengthPassed)
+        {
             this.busId = busId;
             this.state = state;
             this.lengthPassed = lengthPassed;
         }
     }
-    
-    private final List<BusInfo> buses; 
 
-    public BusStateEvent(String sender, List<BusInfo> buses) {
+    private final List<BusInfo> buses;
+
+    public BusStateEvent(String sender, List<BusInfo> buses)
+    {
         super(sender);
         this.buses = buses;
     }
 
     @Override
-    public void visit(AbstractEventProcessor eventProcessor) {
+    public void visit(AbstractEventProcessor eventProcessor)
+    {
         eventProcessor.process(this);
     }
-    
+
+    public List<BusInfo> getBuses()
+    {
+        return buses;
+    }
 }
