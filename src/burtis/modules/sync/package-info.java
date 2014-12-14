f5f45d710a3329881f87fe5b12c5a60d8332cfb0
Amadeusz Sadowski <amis.tau.92@gmail.com>
2014-12-14 20:37:38 +0100

    described packages, removed CommServer

--- src/burtis/modules/sync/package-info.java
+++ src/burtis/modules/sync/package-info.java
@@ -2,6 +2,10 @@
  * 
  */
 /**
+ * This module controls time flow of simulation, dictating clock ticks to all
+ * other modules. Additionally, it checks whether modules described as critical
+ * don't lag behind - if that's the case, simulation is terminated immediately.
+ * 
  * @author Mikołaj Sowiński
  *
  */
