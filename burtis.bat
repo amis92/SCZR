a816eb414dc5feeb68b2865931ed9763ef347d14
Amadeusz Sadowski <amis.tau.92@gmail.com>
2014-12-17 14:25:17 +0100

    jar and bat done

---
+++ burtis.bat
@@ -0,0 +1,6 @@
+start java -cp burtis.jar burtis.modules.network.server.DemoServerModule
+start java -cp burtis.jar burtis.modules.simulation.Simulation
+start java -cp burtis.jar burtis.modules.passengers.PassengerModule
+start java -cp burtis.jar burtis.modules.sync.SynchronizationModule
+start java -cp burtis.jar burtis.modules.network.busscheduler.BusSchedulerModule
+start java -cp burtis.jar burtis.modules.gui.GuiModule
