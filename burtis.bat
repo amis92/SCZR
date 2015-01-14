db07e4e0671a33b60b153b731743c17c862cc864
Amadeusz Sadowski <amis.tau.92@gmail.com>
2015-01-14 11:20:09 +0100

    batch start order change

--- burtis.bat
+++ burtis.bat
@@ -1,6 +1,6 @@
 ﻿@ECHO OFF
 chcp 65001
-FOR %%A IN ("network.server.DemoServerModule","simulation.Simulation","passengers.PassengerModule","sync.SynchronizationModule","busscheduler.BusSchedulerModule","gui.GuiModule") DO (
+FOR %%A IN ("network.server.DemoServerModule","sync.SynchronizationModule","simulation.Simulation","passengers.PassengerModule","busscheduler.BusSchedulerModule","gui.GuiModule") DO (
 	START "%%A" java -cp burtis.jar -Dfile.encoding=UTF-8 burtis.modules.%%A
 	TIMEOUT /t 1
 )
