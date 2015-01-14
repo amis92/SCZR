ee18c0a99c849493e6219083f35f81e9afabe9f1
Amadeusz Sadowski <amis.tau.92@gmail.com>
2015-01-14 15:41:20 +0100

    added bash script to run on linux too

---
+++ burtis.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/sh
+for module in "network.server.DemoServerModule","sync.SynchronizationModule","simulation.Simulation","passengers.PassengerModule","busscheduler.BusSchedulerModule","gui.GuiModule"
+do
+	x-terminal-emulator -e java -cp burtis.jar burtis.modules.$module
+done
